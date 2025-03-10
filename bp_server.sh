58016f1377231d6145394513ff633f9f6fe59ced
log-esh-bug <logeshwaranoffl@gmail.com>
2025-03-10 18:54:01 +0530

    Issues related with pid file must be taken care

--- bp_server.sh
+++ bp_server.sh
@@ -19,8 +19,9 @@ drop_lock(){
 start_backend_helper(){
 	fetch_lock ${1}.pid
 
-	if [ -e ${1}.pid ];then
-		local pid=$(cat ${1}.pid)
+	local pid_file=${PARENT_DIR}/${1}.pid
+	if [ -e ${pid_file} ];then
+		local pid=$(cat ${pid_file})
 	    if [[ $(ps -p $pid --format comm=) == "${1}.sh" ]];then
 			echo "${1} already started!"
 			drop_lock ${1}.pid
@@ -29,8 +30,7 @@ start_backend_helper(){
 	fi
 	echo "${1} Started and will happen for every $2!"
 	${PARENT_DIR}/${1}.sh ${2}&
-	echo "$!" > ${1}.pid
-
+	echo "$!" > ${pid_file}
 	drop_lock ${1}.pid
 }
 
@@ -38,16 +38,17 @@ start_backend_helper(){
 stop_backend_helper(){
 	fetch_lock ${1}.pid
 
-	if [ -e ${1}.pid ];then
-		local pid=$(cat ${1}.pid) 
+	local pid_file=${PARENT_DIR}/${1}.pid
+	if [ -e ${pid_file} ];then
+		local pid=$(cat ${pid_file}) 
 		if [[ $(ps -p $pid --format comm=) == "${1}.sh" ]];then
 			kill -9 $pid
-			rm ${1}.pid
+			rm ${pid_file}
 			echo "${1} Stopped!"
 			drop_lock ${1}.pid
 			return
 		else
-			rm ${1}.pid
+			rm ${pid_file}
 			echo "${1}.pid file contains corrupted pid!"
 		fi
 	fi
@@ -66,10 +67,10 @@ fi
 
 case $1 in
 	start_bp)
-		start_backend_helper ${PARENT_DIR}/start_bp $BACKUP_SLEEP_TIME
+		start_backend_helper start_bp $BACKUP_SLEEP_TIME
 		;;
 	stop_bp)
-		stop_backend_helper ${PARENT_DIR}/start_bp
+		stop_backend_helper start_bp
 		;;
 	*)
 		$LOG_SCRIPT "Invalid option From Primary Server! $1"
