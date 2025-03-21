d2a48022412439aef743fae6681c011acaf1f491
log-esh-bug <logeshwaranoffl@gmail.com>
2025-03-21 18:07:52 +0530

    deleted:    bp_properties.sh 	modified:   bp_server.sh

--- bp_server.sh
+++ bp_server.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 PARENT_DIR=/home/test2/backup_class
-source ${PARENT_DIR}/bp_properties.sh
+BACKUP_DIR=${PARENT_DIR}/repo
+LOCK_DIR=${PARENT_DIR}/locks
+
+#Log related stuffs
+LOG_FILE=/home/sas/pg_repo/script_log
+
+log(){
+    echo "$(date +%F' '%T' '%Z) [$(ps -p $PPID --format comm=) $PPID] LOG: $1" >> $LOG_FILE
+}
+
+
+#Lock Routines
+fetch_lock(){
+	while [ -e ${LOCK_DIR}/$(basename $1).lock ];
+	do
+		sleep 1		
+	done
+	touch ${LOCK_DIR}/$(basename $1).lock 
+}
+
+drop_lock(){
+	if [ -e ${LOCK_DIR}/$(basename $1).lock  ];then
+		rm ${LOCK_DIR}/$(basename $1).lock 
+	fi
+}
 
 start_backend_helper(){
 	fetch_lock ${1}.pid
@@ -9,12 +33,12 @@ start_backend_helper(){
 	if [ -e ${pid_file} ];then
 		local pid=$(cat ${pid_file})
 	    if [[ $(ps -p $pid --format comm=) == "${1}.sh" ]];then
-			$LOG_SCRIPT "${1} already started!"
+			log "${1} already started!"
 			drop_lock ${1}.pid
 			return
 		fi
 	fi
-	$LOG_SCRIPT "${1} Started and will happen for every $2!"
+	log "${1} Started and will happen for every $2!"
 	${PARENT_DIR}/${1}.sh ${2}&
 	echo "$!" > ${pid_file}
 	drop_lock ${1}.pid
@@ -30,35 +54,38 @@ stop_backend_helper(){
 		if [[ $(ps -p $pid --format comm=) == "${1}.sh" ]];then
 			kill -9 $pid
 			rm ${pid_file}
-			$LOG_SCRIPT "${1} Stopped!"
+			log "${1} Stopped!"
 			drop_lock ${1}.pid
 			return
 		else
 			rm ${pid_file}
-			$LOG_SCRIPT "${1}.pid file contains corrupted pid!"
+			log "${1}.pid file contains corrupted pid!"
 		fi
 	fi
 	drop_lock ${1}.pid
-	$LOG_SCRIPT "${1} not started already. First start one!"
+	log "${1} not started already. First start one!"
 }
+
+
+#################### Main ####################
 
 if [ ! -d $LOCK_DIR ];then
 	mkdir $LOCK_DIR
 fi
 
 if [ $# -eq 0 ];then
-	$LOG_SCRIPT "No options provided!"
+	log "No options provided!"
 	exit
 fi
 
 case $1 in
 	start_bp)
-		start_backend_helper start_bp $BACKUP_SLEEP_TIME
+		start_backend_helper start_bp $2
 		;;
 	stop_bp)
 		stop_backend_helper start_bp
 		;;
 	*)
-		$LOG_SCRIPT "Invalid option From Primary Server! $1"
+		log "Invalid option From Primary Server! $1"
 		;;
 esac
